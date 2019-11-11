9f84f314c0bc52bb7d0e8cd82772f42236a089b3
Senku <dorosario.enzo@protonmail.com>
2019-11-11 01:58:27 +0100

    initial commit

---
+++ src/views/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Header from 'components/Header';
+
+const Main: React.FC = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+};
+
+export default Main;
