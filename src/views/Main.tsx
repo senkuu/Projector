e26444e9fecd5bffdf82ce356ef4470a5cdac6d8
Senku <dorosario.enzo@protonmail.com>
2019-11-11 02:01:38 +0100

    clear import

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import Header from 'components/Header';
 
 const Main: React.FC = () => {
